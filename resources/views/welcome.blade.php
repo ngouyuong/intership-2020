c67d1afbc846224a8e94e395d799e26ee78bf8e6
@UyUong <ngouyoung@yahoo.com>
2020-07-22 15:48:02 +0700

    message

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -24,11 +24,6 @@
                 height: 100vh;
             }
 
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
 
             .position-ref {
                 position: relative;
